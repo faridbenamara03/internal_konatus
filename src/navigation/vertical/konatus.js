63942cebffc91b1142deb8a73c021ef4c7980be7
RabbitTerra <rabbit@nftmall.io>
2022-06-27 10:19:55 -0500

    feat: initial commit

---
+++ src/navigation/vertical/konatus.js
@@ -0,0 +1,129 @@
+export default [
+  {
+    search: true,
+  },
+  {
+    title: 'Boston Dynamics',
+    resource: 'konatus',
+    isOperation: true,
+    children: [
+      {
+        title: 'Consumer Robots',
+        icon: 'HomeIcon',
+        children: [
+          {
+            title: 'Quadruped robot',
+            children: [
+              {
+                title: 'New format',
+              },
+              {
+                title: 'Enhanced motricity',
+              },
+              {
+                title: 'Enhanced authono...',
+              },
+              {
+                title: 'Dual sourcing for Quadruped',
+              },
+            ],
+          },
+          {
+            title: 'Hardened quadruped ...',
+          },
+          {
+            title: 'Handling robot',
+          },
+          {
+            title: 'Power and programin...',
+          },
+        ],
+      },
+      {
+        title: 'Military Robots',
+        icon: 'HomeIcon',
+        children: [
+          {
+            title: 'Attacking robot',
+            children: [
+              {
+                title: 'Rocket lancher',
+              },
+              {
+                title: 'Enhanced motricity',
+              },
+              {
+                title: 'Enhanced authono...',
+              },
+              {
+                title: 'Dual sourcing for Quadruped',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Organization Dynamics',
+    resource: 'konatus',
+    isOperation: false,
+    children: [
+      {
+        title: 'Consumer Robots',
+        icon: 'HomeIcon',
+        children: [
+          {
+            title: 'Quadruped robot',
+            children: [
+              {
+                title: 'New format',
+              },
+              {
+                title: 'Enhanced motricity',
+              },
+              {
+                title: 'Enhanced authono...',
+              },
+              {
+                title: 'Dual sourcing for Quadruped',
+              },
+            ],
+          },
+          {
+            title: 'Hardened quadruped ...',
+          },
+          {
+            title: 'Handling robot',
+          },
+          {
+            title: 'Power and programin...',
+          },
+        ],
+      },
+      {
+        title: 'Military Robots',
+        icon: 'HomeIcon',
+        children: [
+          {
+            title: 'Attacking robot',
+            children: [
+              {
+                title: 'Rocket lancher',
+              },
+              {
+                title: 'Enhanced motricity',
+              },
+              {
+                title: 'Enhanced authono...',
+              },
+              {
+                title: 'Dual sourcing for Quadruped',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]
