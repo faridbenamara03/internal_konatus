accd8b24bc96788267c21d5c93aa4e03771f4761
RabbitTerra <rabbit@nftmall.io>
2022-06-27 10:20:05 -0500

    feat: update sidebar

---
+++ src/@core/layouts/layout-vertical/components/vertical-nav-menu/components/vertical-nav-menu-items/VerticalNavMenuItems.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul>
+    <component
+      :is="resolveNavItemComponent(item)"
+      v-for="item in items"
+      :key="item.header || item.title"
+      :item="item"
+      :is-active="isActive"
+      @toggleChange="toggleChange"
+    />
+  </ul>
+</template>
+
+<script>
+import { resolveVerticalNavMenuItemComponent as resolveNavItemComponent } from '@core/layouts/utils'
+import { provide, ref } from '@vue/composition-api'
+import VerticalNavMenuHeader from '../vertical-nav-menu-header'
+import VerticalNavMenuLink from '../vertical-nav-menu-link/VerticalNavMenuLink.vue'
+import VerticalNavMenuSearch from '../vertical-nav-menu-search/VerticalNavMenuSearch.vue'
+import VerticalNavMenuGroup from '../vertical-nav-menu-group/VerticalNavMenuGroup.vue'
+
+export default {
+  components: {
+    VerticalNavMenuHeader,
+    VerticalNavMenuLink,
+    VerticalNavMenuSearch,
+    VerticalNavMenuGroup,
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    provide('openGroups', ref([]))
+    const isActive = ref(true)
+
+    return {
+      resolveNavItemComponent,
+      // toggle
+      isActive,
+    }
+  },
+  methods: {
+    toggleChange(state) {
+      this.isActive = state
+    },
+  },
+}
+</script>
---
+++ src/@core/layouts/layout-vertical/components/vertical-nav-menu/components/vertical-nav-menu-search/VerticalNavMenuSearch.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="control-nav">
+    <b-input-group class="input-group">
+      <b-form-input
+        v-model="searchQuery"
+        placeholder="Search"
+      />
+      <b-input-group-prepend class="input-prepend">
+        <b-icon icon="search" />
+      </b-input-group-prepend>
+    </b-input-group>
+    <div class="mt-1">
+      <b-button-group class="button-group">
+        <b-button
+          :class="{'active': isActive}"
+          @click="handleToggle(true)"
+        >
+          Operation
+        </b-button>
+        <b-button
+          :class="{'active': !isActive}"
+          @click="handleToggle(false)"
+        >
+          organization
+        </b-button>
+      </b-button-group>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  BButton, BButtonGroup, BFormInput, BInputGroup, BInputGroupPrepend,
+} from 'bootstrap-vue'
+
+export default {
+  name: 'VerticalNavMenuSearch',
+  components: {
+    BButton,
+    BButtonGroup,
+    BFormInput,
+    BInputGroup,
+    BInputGroupPrepend,
+  },
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+    isActive: Boolean,
+    searchQuery: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    handleToggle(state) {
+      this.$emit('toggleChange', state)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~@core/scss/base/bootstrap-extended/include';
+@import '~@core/scss/base/components/variables-dark';
+  .control-nav {
+    margin: 16px;
+  }
+  .input-group {
+    border: 1px solid $theme-dark-input-border-color;
+    border-radius: 5px;
+    height: 40px;
+    input {
+      padding: 10px;
+      background: transparent;
+      border: none;
+      height: 100%;
+    }
+    .input-prepend {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 11px;
+    }
+  }
+  .button-group {
+    border: 1px solid $theme-dark-input-border-color;
+    border-radius: 5px;
+    height: 40px;
+    width: 100%;
+    button {
+      background: transparent !important;
+      color: $theme-dark-button-primary-normal-text;
+      cursor: pointer;
+      text-transform: uppercase;
+      font-weight: 500;
+      font-size: 12px;
+      line-height: 24px;
+      width: 50%;
+      justify-content: center;
+      align-items: center;
+      display: flex;
+      border: none;
+      &:nth-child(1) {
+        border-right: 1px solid $theme-dark-input-border-color;
+        border-radius: 5px 0px 0px 5px;
+      }
+      &.active {
+        color: $theme-dark-button-primary-active-text;
+        background: $theme-dark-button-primary-active-bg !important;
+      }
+      &:hover {
+        box-shadow: none !important;
+      }
+    }
+  }
+</style>
