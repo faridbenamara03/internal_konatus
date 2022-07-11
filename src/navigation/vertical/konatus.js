0ff8b773297fddf6129b05e12294057f66e07f8a
RabbitTerra <rabbit@nftmall.io>
2022-07-11 21:22:26 +0800

    chore: add org type to sidebar routes

--- src/navigation/vertical/konatus.js
+++ src/navigation/vertical/konatus.js
@@ -6,57 +6,73 @@ export default [
     title: 'Boston Dynamics',
     resource: 'konatus',
     isOperation: true,
+    type: 'root',
     children: [
       {
         title: 'Consumer Robots',
         icon: 'HomeIcon',
+        type: 'portfolio',
         children: [
           {
             title: 'Quadruped robot',
+            type: 'program',
             children: [
               {
                 title: 'New format',
+                type: 'project',
               },
               {
                 title: 'Enhanced motricity',
+                type: 'project',
               },
               {
                 title: 'Enhanced authono...',
+                type: 'project',
               },
               {
                 title: 'Dual sourcing for Quadruped',
+                type: 'project',
               },
             ],
           },
           {
             title: 'Hardened quadruped ...',
+            type: 'program',
           },
           {
             title: 'Handling robot',
+            type: 'program',
           },
           {
             title: 'Power and programin...',
+            type: 'program',
           },
         ],
       },
       {
         title: 'Military Robots',
         icon: 'HomeIcon',
+        type: 'portfolio',
         children: [
           {
             title: 'Attacking robot',
+            type: 'program',
             children: [
               {
                 title: 'Rocket lancher',
+                type: 'project',
               },
               {
                 title: 'Enhanced motricity',
+                type: 'project',
               },
               {
                 title: 'Enhanced authono...',
+                type: 'project',
               },
               {
                 title: 'Dual sourcing for Quadruped',
+                type: 'project',
               },
             ],
           },
