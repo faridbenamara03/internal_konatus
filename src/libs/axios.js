46dd45b76ed02cbbc9e1de55aea1c04e2ba89f96
hackykitty <talulawilder1@gmail.com>
2022-10-05 16:12:12 +0800

    chore: logo and meta title

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: 'https://some-domain.com/api/',
+  baseURL: 'http://185.151.213.119:8012/',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
