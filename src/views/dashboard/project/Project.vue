5aa41c0167454d2b59abd422201562a75d6277b5
hackykitty <talulawilder1@gmail.com>
2022-12-15 18:15:14 +0900

    fix: redundant today label without range picker

--- src/views/dashboard/project/Project.vue
+++ src/views/dashboard/project/Project.vue
@@ -74,10 +74,10 @@
         </b-tab>
         <template #tabs-end>
           <div class="d-flex ml-auto justify-content-end align-items-center pt-1 pb-1 actions">
-            <div class="d-flex align-items-center">
+            <!-- <div class="d-flex align-items-center">
               <feather-icon icon="CalendarIcon" size="16" style="margin-right:3px" />
               <div style="white-space:nowrap">{{ getToday() }}</div>
-              <!-- <div class="ml-1">
+              <div class="ml-1">
                 <b-form-input style="width:160px" id="popover-manual-1" readonly v-model="selectedMonth"/>
                 <b-popover
                   placement="bottomleft"
@@ -94,8 +94,8 @@
                     </div>
                   </div>
                 </b-popover>
-              </div> -->
-            </div>
+              </div>
+            </div> -->
             <b-button-group v-if="(tabIndex === 1)" class="ml-1">
               <b-button variant="outline-primary">
                 Cost
