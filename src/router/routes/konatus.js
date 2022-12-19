0d499fda00e8eb572314322640a27a376ae82759
hackykitty <talulawilder1@gmail.com>
2022-12-19 18:13:31 +0900

    fix: router operation

--- src/router/routes/konatus.js
+++ src/router/routes/konatus.js
@@ -1,68 +1,68 @@
 export default [
   {
-    path: '/portfolio/:portfolioId/program/:programId/project/:projectId',
+    path: '/operation/portfolio/:portfolioId/program/:programId/project/:projectId',
     name: 'project-view',
     component: () => import('@/views/dashboard/project/Project.vue'),
   },
 
   {
-    path: '/portfolio/:portfolioId/program/:programId',
+    path: '/operation/portfolio/:portfolioId/program/:programId',
     name: 'program-view',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/program/:programId/demand-table',
+    path: '/operation/portfolio/:portfolioId/program/:programId/demand-table',
     name: 'demand-table-view',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/program/:programId/demand-chart',
+    path: '/operation/portfolio/:portfolioId/program/:programId/demand-chart',
     name: 'demand-chart-view',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/program/:programId/reporting-cost',
+    path: '/operation/portfolio/:portfolioId/program/:programId/reporting-cost',
     name: 'demand-reporting-cost',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/program/:programId/reporting-plan',
+    path: '/operation/portfolio/:portfolioId/program/:programId/reporting-plan',
     name: 'demand-reporting-plan',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/program/:programId/control',
+    path: '/operation/portfolio/:portfolioId/program/:programId/control',
     name: 'demand-control',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
 
   {
-    path: '/portfolio/:portfolioId',
+    path: '/operation/portfolio/:portfolioId',
     name: 'portfolio-view',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/demand-table',
+    path: '/operation/portfolio/:portfolioId/demand-table',
     name: 'portfolio-table-view',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/demand-chart',
+    path: '/operation/portfolio/:portfolioId/demand-chart',
     name: 'portfolio-chart-view',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/reporting-cost',
+    path: '/operation/portfolio/:portfolioId/reporting-cost',
     name: 'portfolio-reporting-cost',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/reporting-plan',
+    path: '/operation/portfolio/:portfolioId/reporting-plan',
     name: 'portfolio-reporting-plan',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
   {
-    path: '/portfolio/:portfolioId/control',
+    path: '/operation/portfolio/:portfolioId/control',
     name: 'portfolio-control',
     component: () => import('@/views/dashboard/portfolio/Portfolio.vue'),
   },
