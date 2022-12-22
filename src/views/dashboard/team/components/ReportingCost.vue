6b8b8dab82c3cb608a5aff562198ecece13a7504
hackykitty <talulawilder1@gmail.com>
2022-12-22 20:03:51 +0900

    feat: team reporting cost view 1.1

--- src/views/dashboard/team/components/ReportingCost.vue
+++ src/views/dashboard/team/components/ReportingCost.vue
@@ -1,15 +1,8 @@
-g<template>
-  <div class="demand-view" >
+<template>
+  <div class="demand-view">
     <div class="w-100">
-      <b-card
-        class="chart-card"
-      >
-        <vue-apex-charts
-          type="line"
-          height="350"
-          :options="chartOptions"
-          :series="series"
-        />
+      <b-card class="chart-card">
+        <vue-apex-charts type="line" height="400" :options="chartOptions" :series="series" />
       </b-card>
     </div>
   </div>
@@ -42,120 +35,280 @@ export default {
   },
   data() {
     return {
+      // series: [
+      //   {
+      //     name: "BUDGET ENGAGED",
+      //     type: 'column',
+      //     data: [1000, 500, 1700, 300]
+      //   },
+      //   {
+      //     name: "REAL ESTIMATED",
+      //     type: 'column',
+      //     data: [1800, 1200, 1000, 1500]
+      //   },
+      //   {
+      //     name: "QUOTE",
+      //     type: 'column',
+      //     data: [1300, 800, 2100, 1400]
+      //   },
+      //   {
+      //     name: "DEMAND",
+      //     type: 'column',
+      //     data: [300, 500, 1600, 200]
+      //   },
+      //   {
+      //     name: 'ESTIMATED',
+      //     type: 'line',
+      //     data: [2000, 2000, 2000, 2000]
+      //   }, {
+      //     name: 'ENGAGED',
+      //     type: 'line',
+      //     data: [2800, 2800, 2800, 2800]
+      //   }, {
+      //     name: 'SPENT',
+      //     type: 'line',
+      //     data: [1600, 1600, 1600, 1600]
+      //   }
+      // ],
+      // chartOptions: {
+      //   legend: {
+      //     show: false,
+      //     labels: {
+      //       colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
+      //     },
+      //     customLegendItems: [],
+      //     position: 'bottom',
+      //     horizontalAlign: 'left',
+      //     offsetX: 0,
+      //     offsetY: 10
+      //   },
+      //   chart: {
+      //     height: 350,
+      //     type: 'line',
+      //   },
+      //   plotOptions: {
+      //     bar: {
+      //       horizontal: false,
+      //       columnWidth: '155%',
+      //       borderRadius: 10
+      //     },
+      //   },
+      //   colors: ["green", "red", "yellow", "purple", "blue", "darkgreen", "orange"],
+      //   stroke: {
+      //     width: [1, 1, 2, 2, 2, 1, 1],
+      //     curve: 'straight',
+      //     dashArray: [0, 0, 0, 0, 8, 8, 8]
+      //   },
+      //   title: {
+      //     text: 'CONSUMER ROBOTS',
+      //     style: {
+      //       color: '#ffffff'
+      //     }
+      //   },
+      //   dataLabels: {
+      //     enabled: false
+      //   },
+      //   grid: {
+      //     borderColor: '#B9B9C320',
+      //   },
+      //   // markers: {
+      //   //   discrete: [{
+      //   //     seriesIndex: 3,
+      //   //     dataPointIndex: 11,
+      //   //     fillColor: '#ff0000',
+      //   //     strokeColor: '#ff0000',
+      //   //     size: 4,
+      //   //     radius: 5,
+      //   //     shape: "square" // "circle" | "square" | "rect"
+      //   //   }, {
+      //   //     seriesIndex: 4,
+      //   //     dataPointIndex: 11,
+      //   //     fillColor: '#20C997',
+      //   //     strokeColor: '#20C997',
+      //   //     size: 4,
+      //   //     shape: "circle" // "circle" | "square" | "rect"
+      //   //   }]
+      //   // },
+      //   fill: {
+      //     colors: ['#20C997', '#D46D6D', '#0000FF', '#0000FF', '#0000FF', '#7367F0', '#F5A623']
+      //   },
+      //   labels: ['Sep/21', 'Oct/21', 'Nov/21', 'Dev/21'],
+      //   xaxis: {
+      //     labels: {
+      //       style: {
+      //         colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
+      //       }
+      //     }
+      //   },
+      //   yaxis: {
+      //     labels: {
+      //       style: {
+      //         colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
+      //       },
+      //       formatter(value) {
+      //         if (value < 1000) return value
+      //         return `${value / 1000}k`
+      //       },
+      //     }
+      //   }
+      // },
       series: [
         {
-          name: "BUDGET1",
+          name: "BUDGET ENGAGED",
           type: 'column',
-          data: [0, 0, 1700, 0]
+          data: [null, 1000, 500, 1700, 300, null]
         },
         {
-          name: "BUDGET2",
+          name: "REAL ESTIMATED",
           type: 'column',
-          data: [0, 0, 1600, 0]
+          data: [null, 1800, 1200, 1000, 1500, null]
         },
         {
-          name: 'BUDGET DEMAND',
+          name: "QUOTE",
+          type: 'column',
+          data: [null, 1300, 800, 2100, 1400, null]
+        },
+        {
+          name: "DEMAND",
+          type: 'column',
+          data: [null, 300, 500, 1600, 200, null]
+        },
+        {
+          name: 'ESTIMATED',
           type: 'line',
-          data: [2000, 2000, 2000, 2000]
+          data: [2000, 2000, 2000, 2000, 2000, 2000]
         }, {
-          name: 'BUDGET ENGAGED',
+          name: 'ENGAGED',
           type: 'line',
-          data: [2800, 2600, 2700, 2800]
+          data: [2800, 2800, 2800, 2800, 2800, 2800]
         }, {
-          name: 'BUDGET ESTIMATED',
+          name: 'SPENT',
           type: 'line',
-          data: [1600, 1400, 1200, 1500]
-        },
-        {
-          name: "BUDGET3",
-          type: 'column',
-          data: [0, 0, 1800, 0, 1000, 1500]
-        },
-        {
-          name: "BUDGET4",
-          type: 'column',
-          data: [0, 0, 2100, 0]
-        }],
+          data: [1600, 1600, 1600, 1600, 1600, 1600]
+        }
+      ],
       chartOptions: {
-        // legend: {
-        //   show: false,
-        //   labels: {
-        //     colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
-        //   },
-        //   customLegendItems: [],
-        //   position: 'bottom',
-        //   horizontalAlign: 'left',
-        //   offsetX: 0,
-        //   offsetY: 10
-        // },
-        // chart: {
-        //   height: 350,
-        //   type: 'line',
-        // },
-        // plotOptions: {
-        //   bar: {
-        //     horizontal: false,
-        //     columnWidth: '155%',
-        //     endingShape: 'rounded'
-        //   },
-        // },
-        // colors: ["#20C997", "#D46D6D", "#20C997", "#20C997", "#20C997", "#7367F0", "F5A623"],
-        stroke: {
-          width: [1, 1, 2, 2, 2, 1, 1],
-          curve: 'straight',
-          dashArray: [0, 0, 0, 0, 0, 0, 0]
+        chart: {
+          height: 500,
+          type: 'line',
+          stacked: false
         },
-        // title: {
-        //   text: 'CONSUMER ROBOTS',
-        //   style: {
-        //     color: '#ffffff'
-        //   }
-        // },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '80%',
+            borderRadius: 10
+          },
+        },
         dataLabels: {
-          enabled: true
+          enabled: false
         },
-        grid: {
-          borderColor: '#B9B9C3',
+        stroke: {
+          width: [10, 10, 10, 10, 4, 4, 4],
+          dashArray: [0, 0, 0, 0, 8, 8, 8],
+          colors: ["#FFFFFF00", "#FFFFFF00", "#FFFFFF00", "#FFFFFF00", "#7E36AF", "#008ffb", "#00e396"],
         },
-        markers: {
-          discrete: [{
-            seriesIndex: 3,
-            dataPointIndex: 11,
-            fillColor: '#ff0000',
-            strokeColor: '#ff0000',
-            size: 4,
-            radius: 5,
-            shape: "square" // "circle" | "square" | "rect"
-          }, {
-            seriesIndex: 4,
-            dataPointIndex: 11,
-            fillColor: '#20C997',
-            strokeColor: '#20C997',
-            size: 4,
-            shape: "circle" // "circle" | "square" | "rect"
-          }]
+        title: {
+          text: 'Reporting Cost View',
+          align: 'left',
+          offsetX: 110
         },
-        // fill: {
-        //   colors: ['#20C997', '#D46D6D', '#0000FF', '#0000FF', '#0000FF', '#7367F0', '#F5A623']
-        // },
-        labels: ['Sep/21', 'Oct/21', 'Nov/21', 'Dev/21'],
         xaxis: {
+          categories: ['', 'Sep/21', 'Oct/21', 'Nov/21', 'Dev/21', ''],
           labels: {
             style: {
+              fontWeight: 'bold',
               colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
             }
           }
         },
-        yaxis: {
+        yaxis: [
+          {
+            axisTicks: {
+              show: true,
+            },
+            axisBorder: {
+              show: true,
+              color: '#008FFB'
+            },
+            labels: {
+              style: {
+                colors: '#FFFFFF',
+              },
+              formatter(value) {
+                if (value < 1000) return value
+                return `${(value / 1000).toFixed(2)}k`
+              },
+            },
+            // title: {
+            //   text: "Income (thousand crores)",
+            //   style: {
+            //     color: '#008FFB',
+            //   }
+            // },
+            tooltip: {
+              enabled: true
+            }
+          },
+          // {
+          //   seriesName: 'Income',
+          //   opposite: true,
+          //   axisTicks: {
+          //     show: true,
+          //   },
+          //   axisBorder: {
+          //     show: true,
+          //     color: '#00E396'
+          //   },
+          //   labels: {
+          //     style: {
+          //       colors: '#00E396',
+          //     }
+          //   },
+          //   title: {
+          //     text: "Operating Cashflow (thousand crores)",
+          //     style: {
+          //       color: '#00E396',
+          //     }
+          //   },
+          // },
+          // {
+          //   seriesName: 'Revenue',
+          //   opposite: true,
+          //   axisTicks: {
+          //     show: true,
+          //   },
+          //   axisBorder: {
+          //     show: true,
+          //     color: '#FEB019'
+          //   },
+          //   labels: {
+          //     style: {
+          //       colors: '#FEB019',
+          //     },
+          //   },
+          //   title: {
+          //     text: "Revenue (thousand crores)",
+          //     style: {
+          //       color: '#FEB019',
+          //     }
+          //   }
+          // },
+        ],
+        // tooltip: {
+        //   fixed: {
+        //     enabled: true,
+        //     // position: 'topLeft', // topRight, topLeft, bottomRight, bottomLeft
+        //     offsetY: 30,
+        //     offsetX: 60
+        //   },
+        // },
+        legend: {
+          fontWeight: 800,
           labels: {
-            style: {
-              colors: ["#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF"]
-            },
-            formatter(value) {
-              if (value < 1000) return value
-              return `${value / 1000}k`
-            },
-          }
+            colors: '#888888',
+            useSeriesColors: false
+          },
         }
       },
     }
