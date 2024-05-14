28ceaf9484713ca852e684e9cdb89704ca5fbed5
hackykitty <talulawilder1@gmail.com>
2024-05-14 22:36:18 +0800

    fix: update manual update modal-1

--- src/views/dashboard/globalComponent/ManualUpdateModal.vue
+++ src/views/dashboard/globalComponent/ManualUpdateModal.vue
@@ -63,7 +63,7 @@
           />
         </div>
         <div class="col">
-          <label>Fte R</label>
+          <label>Fte R/E</label>
           <b-form-input
             v-model="fteEstimatedData"
             type="number"
@@ -89,9 +89,9 @@
       </div>
       <div class="row">
         <div class="col">
-          <label>Load R/E</label>
+          <label>Spent R/E</label>
           <b-form-input
-            v-model="loadNewEstimatedData"
+            v-model="spentNewEstimatedData"
             type="number"
             @input="handleChange"
           />
@@ -108,23 +108,65 @@
           <label>% acc R/E</label>
           <b-form-input
             v-model="accNewEstimatedData"
+            aria-describedby="input-live-help"
             type="number"
-            @input="handleChange"
+            :disabled="isAcc === 1"
+            @input="handleChangeAccORest(1)"
           />
+          <b-form-text
+            class="muted"
+            id="input-live-help"
+            style="font-size:10px"
+          >
+            Input only one
+          </b-form-text>
         </div>
         <div class="col">
           <label>Rest To Do R/E</label>
           <b-form-input
             v-model="restNewEstimatedData"
+            aria-describedby="input-live-help"
             type="number"
-            @input="handleChange"
+            :disabled="isAcc === 2"
+            @input="handleChangeAccORest(2)"
           />
+          <b-form-text
+            class="muted"
+            id="input-live-help"
+            style="font-size:10px"
+          >
+            Input only one
+          </b-form-text>
         </div>
         <div class="col">
           <b-button
             style="margin-top:20px"
             variant="primary"
             @click="handleCalculate"
+          >
+            Calculate
+          </b-button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col">
+          <label>Start Date R/E</label>
+          <b-form-input
+            v-model="startDateEstimated"
+          />
+        </div>
+        <div class="col">
+          <label>End Date R/E</label>
+          <b-form-input
+            v-model="endDateEstimated"
+            :disabled="true"
+          />
+        </div>
+        <div class="col">
+          <b-button
+            style="margin-top:20px"
+            variant="primary"
+            @click="handleCalculateDate"
           >
             Calculate
           </b-button>
@@ -175,12 +217,15 @@ export default {
       loadEngageData: 0,
       fteEngageDate: 0,
       spentData: 0,
-      loadNewEstimatedData: 0,
+      isAcc: 0,
+      spentNewEstimatedData: 0,
       fteNewEstimatedData: 0,
       accEstimatedData: 0,
       accNewEstimatedData: 0,
       restEstimatedData: 0,
       restNewEstimatedData: 0,
+      startDateEstimated: '',
+      endDateEstimated: '',
       isValid: false,
     }
   },
@@ -197,40 +242,58 @@ export default {
       this.loadEngageData = newWE.load_engage
       this.loadEstimatedData = newWE.load_reel
       this.accEstimatedData = newWE.acc
-      this.spentData = this.loadEstimatedData * this.accEstimatedData
+      this.spentData = parseFloat(this.loadEstimatedData) * (parseFloat(this.accEstimatedData) / 100.0)
       this.fteEngageData = newWE.fte_engage
       this.fteEstimatedData = newWE.fte_reel
-      this.restEstimatedData = this.loadEstimatedData * (1 - this.accEstimatedData)
+      this.restEstimatedData = this.loadEstimatedData * (1 - (parseFloat(this.accEstimatedData) / 100.0))
     },
     hideModal() {
       this.$refs['my-modal'].hide()
     },
     async handleSave() {
+      const loadNewEstimatedData = this.isAcc === 1 ? parseFloat(this.restNewEstimatedData) + parseFloat(this.spentNewEstimatedData)
+        : parseFloat(this.spentNewEstimatedData) / (parseFloat(this.accNewEstimatedData) / 100.0)
       const payloads = {
         we_id: this.selectedWE.id,
         detail_mode: false,
-        load_estimated: this.loadNewEstimatedData,
-        fte_estimated: this.fteNewEstimatedData,
+        load_estimated: loadNewEstimatedData,
         acc: this.accNewEstimatedData,
         rest_todo_estimated: this.restNewEstimatedData
+      }
+      if (this.fteNewEstimatedData !== '') {
+        payloads.fte_estimated = this.fteNewEstimatedData
       }
       // this.$store.commit('globalState/SUBMIT_MANUAL_UPDATE', payloads)
       await this.$store.dispatch('globalState/submit_manual_update', payloads)
       // this.showToast('success', 'Success Update Work Element.')
       this.$refs['my-modal'].hide()
     },
+    handleCalculateDate() {
+
+    },
+    handleChangeAccORest(type) {
+      this.isValid = false
+      if (type === 1) {
+        this.isAcc = 2
+        this.restNewEstimatedData = ''
+      } else if (type === 2) {
+        this.isAcc = 1
+        this.accNewEstimatedData = ''
+      }
+      if (this.accNewEstimatedData === '' && this.restNewEstimatedData === '') this.isAcc = 0
+      if (this.accNewEstimatedData === '' && this.isAcc === 2) this.isAcc = 0
+      if (this.restNewEstimatedData === '' && this.isAcc === 1) this.isAcc = 0
+    },
     handleCalculate() {
-      if (this.accNewEstimatedData === 0) {
-        this.showToast('warning', 'Inputed Values are not valid, please try again')
+      if (this.isAcc === 0) {
+        this.showToast('warning', 'Please input at least one for %acc R/E or Rest To Do R/E')
         this.isValid = false
-      } else if (parseFloat(this.loadNewEstimatedData) === parseFloat(this.spentData) / parseFloat(this.accNewEstimatedData)
-      && parseFloat(this.loadNewEstimatedData) === parseFloat(this.spentData) + parseFloat(this.restNewEstimatedData)
-      && parseFloat(this.restNewEstimatedData) === parseFloat(this.loadNewEstimatedData) * (1 - parseFloat(this.accNewEstimatedData))) {
+      } else if ((this.isAcc === 1 || this.isAcc === 2) && (this.spentNewEstimatedData === '')) {
+        this.showToast('warning', 'Please input spent R/E data')
+        this.isValid = false
+      } else {
         this.showToast('success', 'All Values are valid, ready to Update now')
         this.isValid = true
-      } else {
-        this.showToast('warning', 'Inputed Values are not valid, please try again')
-        this.isValid = false
       }
     },
     handleChange() {
