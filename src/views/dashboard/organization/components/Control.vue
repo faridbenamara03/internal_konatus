3f8de85204978d4af1c5e7c276d0cc6667c98514
ViewKen1 <118438099+ViewKen1@users.noreply.github.com>
2024-06-27 14:34:09 +0200

    modif K sur Control

--- src/views/dashboard/organization/components/Control.vue
+++ src/views/dashboard/organization/components/Control.vue
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+<template>
+  <div
+    style="width:100%;overflow: auto;"
+    :class="{ 'has-chart': isChartView }"
+  >
+    <template v-if="isChartView && !isManageView">
+      <CustomBarChart />
+    </template>
+    <div v-if="!isChartView && !isManageView">
+      <TableComponent />
+    </div>
+    <div v-if="isManageView">
+      <test
+        :unit-id="unitId"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import TableComponent from './sub-component/table.vue'
+import CustomBarChart from './sub-component/customBarChart.vue'
+import test from './sub-component/test2.vue'
+
+export default {
+  components: {
+    TableComponent,
+    CustomBarChart,
+    test
+  },
+  props: {
+    isChartView: {
+      type: Boolean,
+      default: false,
+    },
+    isManageView: {
+      type: Boolean,
+      default: false,
+    },
+    unitId: {
+      type: Number,
+      default: 1,
+    }
+  },
+  watch: {
+    isChartView(newVal) {
+      console.log('isChartView changed to:', newVal)
+    },
+    isManageView(newVal) {
+      console.log('isManageView changed to:', newVal)
+    }
+  }
+}
+</script>
+=======
 <template>
   <div
     style="width:100%;overflow: auto;"
@@ -29,3 +84,4 @@ export default {
   },
 }
 </script>
+>>>>>>> f0631ccccfaa58e062e9d14e7658186752af59b8
